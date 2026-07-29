1436adbbcbcacf842cfb0ad9764914ff4da94305
Abcinar <111087539+Abcinar@users.noreply.github.com>
2026-07-29 16:25:36 +0300

    Create founder_rules.py

---
+++ engine/rules/founder_rules.py
@@ -0,0 +1,2 @@
+RED_FLAGS = ["hardware", "legal", "compliance", "medical", "enterprise sales", "blockchain", "web3", "heavy capital", "factory"]
+GREEN_FLAGS = ["api wrapper", "micro saas", "extension", "plugin", "template", "script", "no-code"]
