56ed04ac18ae2b4104ea585d8ef55cb370aae83c
Abcinar <111087539+Abcinar@users.noreply.github.com>
2026-07-29 16:22:16 +0300

    Create market_rules.py

---
+++ engine/rules/market_rules.py
@@ -0,0 +1,14 @@
+B2B_KEYWORDS = ["enterprise", "team", "b2b", "agency", "client", "business", "company", "employee", "startup", "founder"]
+B2C_KEYWORDS = ["personal", "b2c", "individual", "hobby", "family", "student", "myself", "consumer"]
+
+TARGET_CUSTOMERS = {
+    "Developers": ["developer", "engineer", "programmer", "coder", "devops", "aws"],
+    "Designers": ["designer", "ui", "ux", "figma"],
+    "Marketers": ["marketer", "seo", "agency", "sales"],
+    "Founders": ["founder", "startup", "entrepreneur", "indie hacker"]
+}
+
+MARKET_SIZE_INDICATORS = {
+    "Niche": ["niche", "specific", "specialized", "small team"],
+    "Large": ["enterprise", "global", "millions", "everyone", "massive"]
+}
