fb8eaa2d1f79abc61ba621aced9a5b04482789d7
Abcinar <111087539+Abcinar@users.noreply.github.com>
2026-07-29 16:21:05 +0300

    Create pain_rules.py

---
+++ engine/rules/pain_rules.py
@@ -0,0 +1,10 @@
+PAIN_KEYWORDS = [
+    "hate", "struggle", "annoying", "tired of", "wish there was",
+    "is there a tool", "how do you manage", "takes too long", "expensive",
+    "hard to", "sucks", "frustrating", "issue", "problem"
+]
+
+SOLUTION_KEYWORDS = [
+    "built this", "my solution", "wrote a script", "workaround",
+    "alternative to", "open source alternative", "fixed it by"
+]
