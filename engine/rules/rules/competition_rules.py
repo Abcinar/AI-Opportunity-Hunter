1b4f6f375ed275ac8c9b6a486e2678cd0f5bfb6a
Abcinar <111087539+Abcinar@users.noreply.github.com>
2026-07-29 16:23:22 +0300

    Create competition_rules.py

---
+++ engine/rules/rules/competition_rules.py
@@ -0,0 +1,2 @@
+HIGH_COMPETITION_KEYWORDS = ["alternative to", "better than", "vs", "competitor", "crowded", "saturated", "clone"]
+LOW_COMPETITION_KEYWORDS = ["first of its kind", "no tools for", "couldn't find", "nothing exists", "untapped"]
